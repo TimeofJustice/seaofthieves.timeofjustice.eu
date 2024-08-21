43561ebdc97f745aada2478316b79184dc6e9c51
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2024-08-21 22:41:09 +0200

    Add wiki model

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,3 +6,13 @@ class Page(models.Model):
     path = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     views = models.IntegerField(default=0)
+
+
+class Wiki(models.Model):
+    title = models.CharField(max_length=255, unique=True, primary_key=True)
+    modules = models.JSONField()
+    more = models.ManyToManyField(Page)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
