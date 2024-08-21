80303d03bdcc1ff4cf68ccb6cf10b77e08955cc5
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2024-08-21 22:58:46 +0200

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Page, Wiki
+from .models import Page, Wiki, Column, Row, Module
 
 # Register your models here.
 admin.site.register(Page)
 admin.site.register(Wiki)
+admin.site.register(Column)
+admin.site.register(Row)
+admin.site.register(Module)
