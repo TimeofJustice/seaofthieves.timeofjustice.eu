619e4a3bde3db55f9c631c421e664628317392f5
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2024-08-22 19:30:18 +0200

    Fix env

--- frontend/src/Env.elm
+++ frontend/src/Env.elm
@@ -3,5 +3,4 @@ module Env exposing (baseUrl)
 
 baseUrl : String
 baseUrl =
-    -- "https://seaofthievesbeta.timeofjustice.eu/api"
-    "http://localhost:8000/"
+    "https://seaofthievesbeta.timeofjustice.eu/api"
